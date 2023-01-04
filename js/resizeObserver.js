eb1a39717a96780d8e316a468246c51ab6f55f38
bernhard759 <bernhard.gailer@gmx.de>
2023-01-04 22:35:34 +0100

    rework translateZ calculation

--- js/resizeObserver.js
+++ js/resizeObserver.js
@@ -1,18 +1,15 @@
 (function () {
 
-    const cubeDiv = document.querySelector("div.cube-div");
-    const translateRatio = 550 / 138;
-    console.log(translateRatio)
     /* Resize Observer object */
-
     const observer = new ResizeObserver(entries => {
         entries.forEach(entry => {
-            entry.target.querySelector(".front").style.transform = `translateZ(${entry.contentRect.width / translateRatio}px)`;
-            entry.target.querySelector(".back").style.transform = `rotateX(180deg) translateZ(${entry.contentRect.width / translateRatio}px)`;
-            entry.target.querySelector(".left").style.transform = `rotateY(-90deg) translateZ(${entry.contentRect.width / translateRatio}px)`;
-            entry.target.querySelector(".right").style.transform = `rotateY(90deg) translateZ(${entry.contentRect.width / translateRatio}px)`;
-            entry.target.querySelector(".top").style.transform = `rotateX(90deg) translateZ(${entry.contentRect.width / translateRatio}px)`;
-            entry.target.querySelector(".bot").style.transform = `rotateX(-90deg) translateZ(${entry.contentRect.width / translateRatio}px)`;
+            /* Cube size is half the container div size so we divide by 4 */
+            entry.target.querySelector(".front").style.transform = `translateZ(${entry.contentRect.width / 4}px)`;
+            entry.target.querySelector(".back").style.transform = `rotateX(180deg) translateZ(${entry.contentRect.width / 4}px)`;
+            entry.target.querySelector(".left").style.transform = `rotateY(-90deg) translateZ(${entry.contentRect.width / 4}px)`;
+            entry.target.querySelector(".right").style.transform = `rotateY(90deg) translateZ(${entry.contentRect.width / 4}px)`;
+            entry.target.querySelector(".top").style.transform = `rotateX(90deg) translateZ(${entry.contentRect.width / 4}px)`;
+            entry.target.querySelector(".bot").style.transform = `rotateX(-90deg) translateZ(${entry.contentRect.width / 4}px)`;
         })
     })
 
